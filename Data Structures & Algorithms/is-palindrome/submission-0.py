dee03a50ea3942e38c5bb8f18ad39c047c0d1abc
Yousef <yousefalhuraibi@gmail.com>
2026-06-03 03:21:06 -0400

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        
+        cleaned = ""
+
+        for char in s:
+            if char.isalnum():
+                cleaned += char.lower()
+
+        z = cleaned[::-1]
+
+
+        return cleaned == z
+        
